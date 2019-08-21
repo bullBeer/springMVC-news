914c7e904b729cf2918cfec8b6914438a2b4c45e
bullBear <479514106@qq.com>
2019-08-21 20:27:34 +0800

    更新home.java

--- src/main/java/com/bullBeer/controllers/Home.java
+++ src/main/java/com/bullBeer/controllers/Home.java
@@ -14,6 +14,7 @@ public class Home {
     Map<String, String> result = new HashMap<String, String>();
     result.put("name", "Tom");
     result.put("age", "18");
+    result.put("city", "Shenzhen");
     return result;
   }
 }
